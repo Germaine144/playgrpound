89c260caa49dd5487496085da1b667a198b3f6e9
Germaine144 <umuhiregermaine12@gmail.com>
2025-03-25 16:02:35 +0200

    adding template and arrow functions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-let userName = "user";
+let userName = "German";
 const birthYear = 1999;
 const currentYear = 2025;
+
+let age = currentYear - birthYear;
+let message = `My name is ${userName} and my age is ${age}`;
+
+console.log(message);
+
+const Birth = (age) => {
+    let birthYear = currentYear - age;
+    return `I was born in ${birthYear}`;
+}
+
+console.log(Birth(30));
